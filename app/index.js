c7ae03e217c4ee98666730cd3a4e51c006dd15e2
Zainab Sadaqat <zainabsadaqat74@gmail.com>
2026-01-30 10:14:37 +0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000;
 
+app.get('/', (req, res) => {
+  res.send('<h1>DevSecOps Project Live!</h1><p>Success!</p>');
+});
+
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
