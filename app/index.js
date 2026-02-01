3f38113ec6897059e25bf55047e62998390677e4
Zainab Sadaqat <zainabsadaqat74@gmail.com>
2026-02-01 16:39:54 +0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,9 +1,12 @@
 const http = require('http');
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World! My DevSecOps Pipeline is Successful!');
+  res.end('Hello World! DevSecOps Pipeline is 100% Successful!');
 });
-server.listen(3000, '0.0.0.0', () => {
-  console.log('Server running at http://0.0.0.0:3000/');
+
+const PORT = 3000;
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}/`);
 });
